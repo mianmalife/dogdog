96e23a6fa5fe54723a437a70fc1b31ae7a75e3bc
mianmalife <253590433@qq.com>
2023-02-16 23:17:52 +0800

    ci: base

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ import ElementPlus from "unplugin-element-plus/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/dogdog/",
   plugins: [
     vue(),
     AutoImport({
