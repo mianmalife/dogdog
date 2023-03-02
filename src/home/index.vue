2ccadb60046f52b76dbb7599d8061522ed4d1fe6
mianmalife <253590433@qq.com>
2023-03-02 11:51:16 +0800

    feat: vue-router-v4 init

--- src/home/index.vue
+++ src/home/index.vue
@@ -1,12 +1,39 @@
 <script setup>
 import { ref, onMounted } from "vue";
-const count = ref(0);
+const breeds = ref([]);
+const activeIndex = ref("0");
+const handleSelect = (key, keyPath) => {
+  console.log(key, keyPath);
+  activeIndex.value = key;
+};
 onMounted(() => {
-  count.value++;
+  fetch("https://dog.ceo/api/breeds/list/all").then((response) => {
+    response.json().then((res) => {
+      console.log(res);
+      breeds.value = res.message
+        ? Object.keys(res.message).slice(0, 20) || []
+        : [];
+    });
+  });
 });
 </script>
 <template>
-  <div>
-    {{ count }}
-  </div>
+  <el-container>
+    <el-header>
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        :ellipsis="true"
+        :popper-offset="12"
+        @select="handleSelect"
+      >
+        <el-menu-item index="logo">LOGO</el-menu-item>
+        <el-menu-item v-for="(breed, index) in breeds" :index="String(index)">{{
+          breed
+        }}</el-menu-item>
+      </el-menu></el-header
+    >
+    <el-main> content</el-main>
+  </el-container>
 </template>
