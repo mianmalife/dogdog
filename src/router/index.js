9451d406e1d5a838944531dfccdd993976dcea2d
llf000 <915428940@qq.com>
2023-03-03 22:51:11 +0800

    修改路由历史模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/components/Home.vue";
 import AppLayout from "@/components/layout/AppLayout.vue";
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
