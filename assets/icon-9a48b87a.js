dbfdf344a5e68e722b10c255a86074e2b048ae82
mianmalife <253590433@qq.com>
2023-03-05 22:03:38 +0800

    deploy

--- assets/icon-9a48b87a.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./base-040bad82.js";const i=o([String,Object,Function]);export{i};
---
+++ assets/icon-cad378fb.js
@@ -0,0 +1,1 @@
+import{a as o}from"./base-72e1687a.js";const i=o([String,Object,Function]);export{i};
